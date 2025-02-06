a0126cfb4697b594a150beaac922b99ad4eb64ae
jacobpgarrett <jpg2002@bu.edu>
2025-02-06 11:21:31 -0500

    Update test_newton.py

--- tests/test_newton.py
+++ tests/test_newton.py
@@ -6,14 +6,3 @@ import sympy as sp
 import numpy as np
 import pytest
 
-# Simple test to test that function works in 1D
-def test_simple():
-    guess = 1
-    x = sp.symbols('x')
-    f = lambda x: x
-    df = sp.lambdify(x, sp.diff(f(x), x))
-    f = sp.lambdify(x, f(x))
-    found = newton(guess, f, df)
-    known = 0
-    assert np.isclose(found, known)
-
