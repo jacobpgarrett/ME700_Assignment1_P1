7885891712a316413b9fce1de38e80ca6d56c9a0
jacobpgarrett <jpg2002@bu.edu>
2025-02-15 09:38:52 -0500

    modified:   tests/test_elastic.py

--- tests/test_elastic.py
+++ tests/test_elastic.py
@@ -7,7 +7,7 @@ import numpy as np
 import pytest
 
 def test_zeros():
-    epsilon = 0
+    epsilon = [0, 0.1]
     known = [0, 1]
     [found_iso, found_kin] = stress_behavior(epsilon, 10, 10, 10)
     assert known == found_iso
