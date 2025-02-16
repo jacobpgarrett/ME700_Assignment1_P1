daaffc237cf34f9a0c260dacc06fa3fba79d652a
jacobpgarrett <jpg2002@bu.edu>
2025-02-16 12:12:51 -0500

    modified:   src/elastic/__pycache__/elastic.cpython-312.pyc modified:   src/elastic/elastic.py

--- src/elastic/elastic.py
+++ src/elastic/elastic.py
@@ -1,135 +1,97 @@
 import numpy as np
-import sympy as sp
 import matplotlib.pyplot as mpl
 
-def check_epsilon(epsilon):
-    if not isinstance(epsilon, np.ndarray):
-        raise ValueError('Epsilon must be an array')
+# Define Material Model Class
+class MaterialModel:
+    def __init__(self, epsilon, E, H, Y0):
+        self.epsilon = epsilon # Input strain
+        self.E = E # Input Young's Modulus
+        self.H = H # Input Hardness Modulus
+        self.Y0 = Y0 # Input Yield Stress
+        self.check_epsilon()
+        self.zero_state()
 
-def zero_state(epsilon):
-    if not epsilon[0] == 0:
-        raise ValueError('The initial strain must be 0')
+    # Function to ensure that epsilon is a numpy array
+    def check_epsilon(self):
+        if not isinstance(self.epsilon, np.ndarray):
+            raise ValueError("Epsilon must be an array")
 
-def isotropic(epsilon, E, H, Y0):
+    # Function to ensure that structure begins at 0 stress
+    def zero_state(self):
+        if not self.epsilon[0] == 0:
+            raise ValueError("The initial strain must be 0")
 
-    '''
+    # Different hardening subclasses use different methods
+    def calculate_stress(self):
+        raise NotImplementedError("Subclasses should implement this method")
 
-    This function computes the isotropic stress-strain behavior of a linear element
-    with isotropic hardening.  It returns the stress as a function of strain.
+class IsotropicHardening(MaterialModel):
+    def calculate_stress(self):
 
-    Inputs:
-    epsilon: strain
-    E: Young's modulus
-    H: hardening modulus
-    Y0: initial yield stress
-    
-    Output:
-    sig: stress
+        # Initialize Data vectors
+        sig = np.zeros(len(self.epsilon))
+        Y = self.Y0
+        epsilon_p = np.zeros(len(self.epsilon))
 
-    '''
-    
-    #
+        for i in range(1, len(self.epsilon)):
+            # Elastic Predictor Step
+            Y = self.Y0 + self.H * epsilon_p[i - 1]
+            del_sig_trial = self.E * (self.epsilon[i] - self.epsilon[i - 1])
+            sig_trial = sig[i - 1] + del_sig_trial
+            state = float(abs(sig_trial) - Y)
 
+            if state <= 0:  # elastic
+                sig[i] = sig_trial
+                epsilon_p[i] = epsilon_p[i - 1]
+            else:  # plastic
+                del_epsilon_p = state / (self.E + self.H)
+                sig[i] = sig_trial - np.sign(sig_trial) * self.E * del_epsilon_p
+                epsilon_p[i] = epsilon_p[i - 1] + del_epsilon_p
 
-    # Define initial state
-    sig = np.zeros(len(epsilon)) # Initialize stress vector
-    Y = Y0 # Initial yield stress
-    epsilon_p = np.zeros(len(epsilon)) # Initialize plastic strain vector
+        return sig
 
-    # Loop through each strain increment
-    for i in range(1, len(epsilon)):
-        Y = Y0+H*epsilon_p[i-1] # Updated yield stress
+class KinematicHardening(MaterialModel):
+    def calculate_stress(self):
+        # Initialize Data Vectors
+        sig = np.zeros(len(self.epsilon))
+        alpha = np.zeros(len(self.epsilon))
+        epsilon_p = np.zeros(len(self.epsilon))
 
-        # Elastic Predictor Step
-        del_sig_trial = E*(epsilon[i]-epsilon[i-1]) # Test change in stress assuming elastic
-        sig_trial = sig[i-1]+del_sig_trial # Stress after strain increment
-        state = float(abs(sig_trial)-Y) # Check if over or under yield
-        
-        if state <= 0: # elastic
-            sig[i] = sig_trial # Stress is equal to trial stress
-            epsilon_p[i] = epsilon_p[i-1]
-        else: # plastic
-            del_epsilon_p = state/(E+H) # Plastic strain increment
-            sig[i] = sig_trial - np.sign(sig_trial)*E*del_epsilon_p # Stress after plastic strain
-            epsilon_p[i] = epsilon_p[i-1]+del_epsilon_p # Total plastic strain
-            
-    
-    return sig # Returns stress vector
+        for i in range(1, len(self.epsilon)):
+            # Elastic Predictor Step
+            sig_trial = sig[i - 1] + self.E * (self.epsilon[i] - self.epsilon[i - 1])
+            alpha_trial = alpha[i - 1]
+            eta_trial = sig_trial - alpha_trial
+            state = float(abs(eta_trial) - self.Y0)
 
-def kinematic(epsilon, E, H, Y0):
-    '''
+            if state <= 0:  # elastic
+                sig[i] = sig_trial
+                alpha[i] = alpha_trial
+                epsilon_p[i] = epsilon_p[i - 1]
+            else:  # plastic
+                del_epsilon_p = state / (self.E + self.H)
+                sig[i] = sig_trial - np.sign(eta_trial) * self.E * del_epsilon_p
+                alpha[i] = alpha[i - 1] + np.sign(eta_trial) * self.H * del_epsilon_p
+                epsilon_p[i] += del_epsilon_p
 
-    This function computes the kinematic stress-strain behavior of a linear element
-    with kinematic hardening.  It returns the stress as a function of strain.
-
-    Inputs:
-    epsilon: strain
-    E: Young's modulus
-    H: hardening modulus
-    Y0: initial yield stress
-
-    Output:
-    sig: stress
-
-    '''
-
-    # Define initial state of 0
-    sig = np.zeros(len(epsilon)) # Initialize stress vector
-    alpha = np.zeros(len(epsilon)) # Initialize back stress vector
-    epsilon_p = np.zeros(len(epsilon)) # Initialize plastic strain vector
-
-    for i in range(1, len(epsilon)):
-
-        # Elastic Predictor Step
-        sig_trial = sig[i-1] + E*(epsilon[i]-epsilon[i-1])
-        alpha_trial = alpha[i-1]
-        eta_trial = sig_trial-alpha_trial
-        state = float(abs(eta_trial)-Y0)
-        
-        if state <= 0: # elastic
-            sig[i] = sig_trial
-            alpha[i] = alpha_trial
-            epsilon_p[i] = epsilon_p[i-1]
-        else: # plastic
-            del_epsilon_p = state/(E+H)
-            sig[i] = sig_trial-np.sign(eta_trial)*E*del_epsilon_p
-            alpha[i] = alpha[i-1]+np.sign(eta_trial)*H*del_epsilon_p
-            epsilon_p[i] = epsilon_p[i] + del_epsilon_p
-
-    return sig
+        return sig
 
 def stress_behavior(epsilon, E, H, Y0):
-    '''
-    This function takes strain and material properties as inputs and plots the stress-strain behavior of the material.
-    It will plot and return the stress behavior for different types of hardening models (isotropic and kinematic).
+    # Creates instances of model classes
+    iso_model = IsotropicHardening(epsilon, E, H, Y0)
+    kin_model = KinematicHardening(epsilon, E, H, Y0)
 
-    Inputs:
-    epsilon: strain
-    E: Young's modulus
-    H: hardening modulus
-    Y0: initial yield stress
+    # Calculate stress states
+    stress_isotropic = iso_model.calculate_stress()
+    stress_kinematic = kin_model.calculate_stress()
 
-    Output:
-    stress_isotropic: stress-strain behavior for isotropic hardening
-    stress_kinematic: stress-strain behavior for kinematic hardening
-    '''
-
-    mpl.close() # Closes open MatPlotLib windows
-
-    check_epsilon(epsilon)
-
-    zero_state(epsilon)
-
-    stress_isotropic = isotropic(epsilon, E, H, Y0) # Computes stress for given strains using isotropic hardening
-    stress_kinematic = kinematic(epsilon, E, H, Y0) # Computes stress for given strains using kinematic hardening
-
-    # Plots stress-strain curves
+    # Plots the stress strain behavior of the different stress models
     mpl.plot(epsilon, stress_isotropic, label='Isotropic')
     mpl.plot(epsilon, stress_kinematic, label='Kinematic')
     mpl.xlabel('Strain')
     mpl.ylabel('Stress')
     mpl.legend()
-    mpl.show()
     mpl.title('Stress vs. Strain Behavior for different types of hardening')
+    mpl.show()
 
     return stress_isotropic, stress_kinematic
